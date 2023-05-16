33ec80fb084ed141e21f3a19bb2531b0e0733bf7
aldhairescobar <aldhair.escobar@gmail.com>
2023-05-15 19:41:46 -0600

    create getComputerChoice and return the result

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('hey there');
+const optionsToComputer = ['rock', 'paper', 'scissors']
+
+
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3)
+  let computerChoice = optionsToComputer[randomNumber]
+
+  return computerChoice
+}
