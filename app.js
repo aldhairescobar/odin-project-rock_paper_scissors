4c934cecc8ecbe4d2690456c3f2ac22c7e7bec27
aldhairescobar <aldhair.escobar@gmail.com>
2023-05-15 20:05:30 -0600

    create playRound function and set a user selection

--- app.js
+++ app.js
@@ -7,3 +7,30 @@ function getComputerChoice() {
 
   return computerChoice
 }
+
+function playRound(playerSelection, computerSelection){
+
+  let playerSelectionLowerCase = playerSelection.toLowerCase();
+
+  if(playerSelectionLowerCase === 'rock' && computerSelection === 'paper'){
+    return 'You Lose! Paper beats Rock'
+  } else if(playerSelectionLowerCase === 'rock' && computerSelection === 'scissors'){
+    return 'You Won! Rock beats Scissors'
+  } else if(playerSelectionLowerCase === 'paper' && computerSelection === 'rock') {
+    return 'You Won! Paper beats Rock'
+  } else if(playerSelectionLowerCase === 'paper' && computerSelection === 'scissors'){
+    return 'You Lose! Scissors beats Paper'
+  } else if(playerSelectionLowerCase === 'scissors' && computerSelection === 'rock'){
+    return 'You Lose! Rock beats Scissors'
+  } else if(playerSelectionLowerCase === 'scissors' && computerSelection === 'paper'){
+    return 'You Won! Scissors beats Paper'
+  } else {
+    return 'Draw, try again!'
+  }
+
+}
+
+
+const playerSelection = 'roCk';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
